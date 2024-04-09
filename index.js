ae99e357fc98665fc3e1336778d02d2854c82427
Yan Malashonak <yan.malashonak@gmail.com>
2024-04-09 15:27:17 +0300

    feat: make fetch request

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const puppeteer = require('puppeteer');
   });
 
   const page = await browser.newPage();
-  await page.goto('https://gpk.gov.by');
+  await page.goto('https://gpk.gov.by', {
+    timeout: 60_000,
+    waitUntil: 'domcontentloaded',
+  });
 
-  const response = await page.evaluate(() => {
-    console.log('Console works');
+  await page.evaluate(async () => {
+    const response = await fetch('https://gpk.gov.by/local/ajax/order-archive.php', {
+      method: 'POST',
+      body: new URLSearchParams({
+        ppr: 'brest',
+        date: '08.04.2024',
+      }),
+    });
+
+    const content = await response.json();
+    console.log(content);
   });
 
   await browser.close();
